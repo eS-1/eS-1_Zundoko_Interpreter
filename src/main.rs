ab2cda44cb3680624b321a23b428a31792ced45b
eS-1 <cybergun00@gmail.com>
2020-01-17 23:00:29 +0900

    minor fixes

--- src/main.rs
+++ src/main.rs
@@ -2,68 +2,109 @@ use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+const BUF_SIZE: usize = 10000;
+
+fn main() -> Result<(), Box<dyn std::error::Error>>
+{
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
     let reader = BufReader::new(File::open(file_path)?);
 
-    let mut buf: [u8; 10000] = [0; 10000];
+    let mut buf: [u8; BUF_SIZE] = [0; BUF_SIZE];
     let mut ptr = 0;
     let mut loop_vec = Vec::new();
 
-    for result in reader.lines() {
+    for result in reader.lines()
+    {
         let li = result?;
+        let li_size = li.len();
         let mut index = 0;
-        while index < li.len() {
+        while index < li_size
+        {
             let order = &li[index..index + 18];
-            if order == "ズンズンズン" {
-                if ptr < buf.len() {
+            if order == "ズンズンズン"
+            {
+                if ptr < BUF_SIZE
+                {
                     ptr += 1;
-                } else {
+                }
+                else
+                {
                     eprintln!("ポインタが大きすぎます");
                 }
-            } else if order == "ズンズンドコ" {
-                if ptr > 0 {
+            }
+            else if order == "ズンズンドコ"
+            {
+                if ptr > 0
+                {
                     ptr -= 1;
-                } else {
+                }
+                else
+                {
                     eprintln!("ポインタが負になりました");
                 }
-            } else if order == "ズンドコズン" {
+            }
+            else if order == "ズンドコズン"
+            {
                 buf[ptr] += 1;
-            } else if order == "ズンドコドコ" {
+            }
+            else if order == "ズンドコドコ"
+            {
                 buf[ptr] -= 1;
-            } else if order == "ドコズンズン" {
-                if buf[ptr] == 0 {
+            }
+            else if order == "ドコズンズン"
+            {
+                if buf[ptr] == 0
+                {
                     let mut w_count = 1;
                     index += 18;
-                    while w_count > 0 {
-                        if index >= li.len() {
+                    while w_count > 0
+                    {
+                        if index >= li_size
+                        {
                             eprintln!("ドコズンドコがありません");
                         }
-                        if &li[index..index + 18] == "ドコズンズン" {
+
+                        if &li[index..index + 18] == "ドコズンズン"
+                        {
                             w_count += 1;
-                        } else if &li[index..index + 18] == "ドコズンドコ" {
+                        }
+                        else if &li[index..index + 18] == "ドコズンドコ"
+                        {
                             w_count -= 1;
                         }
                         index += 18;
                     }
-                } else {
+                }
+                else
+                {
                     loop_vec.push(index);
                 }
-            } else if order == "ドコズンドコ" {
-                if buf[ptr] != 0 {
+            }
+            else if order == "ドコズンドコ"
+            {
+                if buf[ptr] != 0
+                {
                     index = loop_vec.pop().unwrap();
                     loop_vec.push(index);
-                } else {
+                }
+                else
+                {
                     loop_vec.pop();
                 }
-            } else if order == "ドコドコズン" {
+            }
+            else if order == "ドコドコズン"
+            {
                 print!("{}", buf[ptr] as char);
-            } else if order == "ドコドコドコ" {
+            }
+            else if order == "ドコドコドコ"
+            {
                 let mut w_input = [0u8; 1];
                 let _ = std::io::stdin().read(&mut w_input);
                 buf[ptr] = w_input[0];
-            } else if order == "キ・ヨ・シ！" {
+            }
+            else if order == "キ・ヨ・シ！"
+            {
                 print!("Hello, World!");
             }
             index += 18;
