a1003ecd15d2b0db33348c0c598ea7072b4b6c30
eS-1 <cybergun00@gmail.com>
2020-01-20 14:57:19 +0900

    refactored inside while

--- src/main.rs
+++ src/main.rs
@@ -21,97 +21,103 @@ fn main() -> Result<(), Box<dyn std::error::Error>>
         let mut index = 0;
         while index < li_size
         {
-            let order = &li[index..index + 18];
-            if order == "ズンズンズン"
+            match &li[index..index + 18]
             {
-                if ptr < BUF_SIZE
+                "ズンズンズン" =>
                 {
-                    ptr += 1;
-                }
-                else
-                {
-                    eprintln!("ポインタが大きすぎます");
-                }
-            }
-            else if order == "ズンズンドコ"
-            {
-                if ptr > 0
-                {
-                    ptr -= 1;
-                }
-                else
-                {
-                    eprintln!("ポインタが負になりました");
-                }
-            }
-            else if order == "ズンドコズン"
-            {
-                buf[ptr] += 1;
-            }
-            else if order == "ズンドコドコ"
-            {
-                buf[ptr] -= 1;
-            }
-            else if order == "ドコズンズン"
-            {
-                match buf[ptr]
-                {
-                    0 =>
+                    if ptr < BUF_SIZE
                     {
-                        let mut w_count = 1;
-                        index += 18;
-                        while w_count > 0
+                        ptr += 1;
+                    }
+                    else
+                    {
+                        eprintln!("ポインタが大きすぎます");
+                    }
+                },
+                "ズンズンドコ" =>
+                {
+                    if ptr > 0
+                    {
+                        ptr -= 1;
+                    }
+                    else
+                    {
+                        eprintln!("ポインタが負になりました");
+                    }
+                },
+                "ズンドコズン" =>
+                {
+                    buf[ptr] += 1;
+                },
+                "ズンドコドコ" =>
+                {
+                    buf[ptr] -= 1;
+                },
+                "ドコズンズン" =>
+                {
+                    match buf[ptr]
+                    {
+                        0 =>
                         {
-                            if index >= li_size
-                            {
-                                eprintln!("ドコズンドコがありません");
-                            }
-
-                            if &li[index..index + 18] == "ドコズンズン"
-                            {
-                                w_count += 1;
-                            }
-                            else if &li[index..index + 18] == "ドコズンドコ"
-                            {
-                                w_count -= 1;
-                            }
+                            let mut w_count = 1;
                             index += 18;
-                        }
-                    },
-                    _ =>
-                    {
-                        loop_vec.push(index);
-                    },
-                }
-            }
-            else if order == "ドコズンドコ"
-            {
-                match buf[ptr]
+                            while w_count > 0
+                            {
+                                if index >= li_size
+                                {
+                                    eprintln!("ドコズンドコがありません");
+                                }
+
+                                if &li[index..index + 18] == "ドコズンズン"
+                                {
+                                    w_count += 1;
+                                }
+                                else if &li[index..index + 18] == "ドコズンドコ"
+                                {
+                                    w_count -= 1;
+                                }
+                                index += 18;
+                            }
+                        },
+                        _ =>
+                        {
+                            loop_vec.push(index);
+                        },
+                    }
+                },
+                "ドコズンドコ" =>
                 {
-                    0 =>
+                    match buf[ptr]
                     {
-                        loop_vec.pop();
-                    },
-                    _ =>
-                    {
-                        index = loop_vec.pop().unwrap();
-                        loop_vec.push(index);
-                    },
-                }
-            }
-            else if order == "ドコドコズン"
-            {
-                print!("{}", buf[ptr] as char);
-            }
-            else if order == "ドコドコドコ"
-            {
-                let mut w_input = [0u8; 1];
-                let _ = std::io::stdin().read(&mut w_input);
-                buf[ptr] = w_input[0];
-            }
-            else if order == "キ・ヨ・シ！"
-            {
-                print!("Hello, World!");
+                        0 =>
+                        {
+                            loop_vec.pop();
+                        },
+                        _ =>
+                        {
+                            index = loop_vec.pop().unwrap();
+                            loop_vec.push(index);
+                        },
+                    }
+                },
+                "ドコドコズン" =>
+                {
+                    print!("{}", buf[ptr] as char);
+                },
+                "ドコドコドコ" =>
+                {
+                    let mut w_input = [0u8; 1];
+                    let _ = std::io::stdin().read(&mut w_input);
+                    buf[ptr] = w_input[0];
+                },
+                "キ・ヨ・シ！" =>
+                {
+                    print!("Hello, World!");
+                },
+                _ =>
+                {
+                    eprintln!("ズンドコできません");
+                },
             }
             index += 18;
         }
